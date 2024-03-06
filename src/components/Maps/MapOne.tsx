934371ebf4410190071f0bc88a4ac5d13f023b48
naimuddin94 <developernaimuddin@gmail.com>
2024-03-06 20:48:54 +0600

    maps issue solved

--- src/components/Maps/MapOne.tsx
+++ src/components/Maps/MapOne.tsx
@@ -39,7 +39,7 @@ const MapOne = () => {
       },
     });
     mapOne;
-  });
+  }, []);
 
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-7">
